8a6ba97ee341376c753efe25b1ccac95d6209b5b
Adrian Cheong <adriancheong@gic.com.sg>
2016-10-08 22:55:34 +0800

    Moved securities generation logic out of this project into a new project SecuritiesPopulater

--- src/SecuritiesService/Program.cs
+++ src/SecuritiesService/Program.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using StackExchange.Redis;
-using System.Reflection;
 using SecuritiesService.Models;
 using SecuritiesService.Helpers;
 
@@ -14,10 +13,6 @@ namespace SecuritiesService
 {
     public class Program
     {
-        private static readonly string REDIS_ALIAS = "MYREDIS";
-        private static readonly string REDIS_ADDRESS_ENV_PROPERTY_KEY = REDIS_ALIAS + "_PORT_6379_TCP_ADDR";
-        private static readonly string REDIS_PORT_ENV_PROPERTY_KEY = REDIS_ALIAS + "_PORT_6379_TCP_PORT";
-        private static readonly int NUMBER_OF_SECURITIES_TO_CREATE = 100000;
 
         public static void Main(string[] args)
         {
@@ -42,39 +37,6 @@ namespace SecuritiesService
                 Console.WriteLine(key + " : " + Environment.GetEnvironmentVariable(key));
             }
             
-        }
-
-        private static void initAndPopulateRedisStore()
-        {
-            var configurationOptions = new ConfigurationOptions
-            {
-                EndPoints =
-                {
-                    { Environment.GetEnvironmentVariable(REDIS_ADDRESS_ENV_PROPERTY_KEY), int.Parse(Environment.GetEnvironmentVariable(REDIS_PORT_ENV_PROPERTY_KEY)) }
-                }
-                //KeepAlive = 180,
-                //Password = password,
-                //DefaultVersion = new Version("2.8.5"),
-                // Needed for cache clear
-                //AllowAdmin = true
-            };
-            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(configurationOptions);
-
-            IDatabase db = redis.GetDatabase();
-            IList<Security> securities = SecuritiesGenerator.GenerateRandomSecurities(NUMBER_OF_SECURITIES_TO_CREATE);
-            foreach (Security security in securities)
-            {
-                db.HashSet(security.SecurityId, GenerateRedisHash<Security>(security));
-            }
-        }
-
-        private static HashEntry[] GenerateRedisHash<T>(T obj)
-        {
-            var props = typeof(T).GetProperties();
-            var hash = new HashEntry[props.Count()];
-            for (int i = 0; i < props.Count(); i++)
-                hash[i] = new HashEntry(props[i].Name, props[i].GetValue(obj).ToString());
-            return hash;
         }
     }
 }
